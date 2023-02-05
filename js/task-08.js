35387d90105552f8ffed796bab33397890b73fbc
Yana <yanalunina96@gmail.com>
2023-02-05 22:56:59 +0200

    done task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formEl = document.querySelector(".login-form");
+console.log(formEl);
 
+formEl.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+  const formData = {
+    mail,
+    password,
+  };
+
+  mail === "" || password === ""
+    ? alert("Всі поля повинні бути заповнені!")
+    : console.log(formData);
+
+  event.currentTarget.reset();
+}
