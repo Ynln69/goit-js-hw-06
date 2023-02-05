d9fa78363b9836ebd28a6680af8e32df440bb76f
Yana <yanalunina96@gmail.com>
2023-02-05 23:06:05 +0200

    done task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
+const btnEl = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+
+btnEl.addEventListener("click", () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = document.body.style.backgroundColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
